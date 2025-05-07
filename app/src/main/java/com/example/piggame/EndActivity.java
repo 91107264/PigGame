8ba2c7bf5164c2eb7ced09a6894c25b57bf09c10
91107264 <91107264@mvusd.net>
2025-05-07 11:17:28 -0700

    functional

---
+++ app/src/main/java/com/example/piggame/EndActivity.java
@@ -0,0 +1,41 @@
+package com.example.piggame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_end);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.end), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button backBtn = findViewById(R.id.button3);
+        backBtn.setOnClickListener(view -> {
+
+            Toast.makeText(this, "You clicked the confirm button!",
+                    Toast.LENGTH_SHORT).show();
+
+            Log.i("Learn", "Back Button Clicked!");
+            startActivity(new Intent(EndActivity.this, PlayerSelect.class));
+        });
+    }
+}
